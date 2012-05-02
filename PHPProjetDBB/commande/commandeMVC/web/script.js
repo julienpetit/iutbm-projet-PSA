1c4724b3428a64b45607f6252de533f5ed1d6507
patrocle <julien.petit.mathay@gmail.com>
2012-05-02 11:21:53 +0200

    * Fonctions de suppression de pièces d'une commande

--- PHPProjetDBB/commande/commandeMVC/web/script.js
+++ PHPProjetDBB/commande/commandeMVC/web/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
 	
+	/**
+	 * Soumission du formulaire
+	 */
+	$("#formulaire").submit(function(e){
+		ajoutChampsCachesSoumission();
+	});
 	
    /**
     * Widget Pièces ----------------------------------------------------------------------------------------
     */
 	
 	// Recherche de pièces
+	/**
+	 * on affiche la nouvelle liste de pièce disponible lors de la modification du champs de recherche de pièces disponibles
+	 */
 	$("input#recherchePiece").keyup(function(){
 		var chaine = $('input#recherchePiece').val();
 		
@@ -35,7 +44,7 @@ $(document).ready(function() {
 			}
 		});
 		
-		addClassPiecesDispo();
+		addClassSelectedPiecesDispo();
 		
 	});
 	
@@ -44,7 +53,14 @@ $(document).ready(function() {
 	 * Ajout d'une pièce à la commande  -----------------------------------------------------------------------
      */
 
-	// click sur une pièce
+	/**
+	 * Lors du click sur une pièce, on affiche un formulaire de validation de la pièce pour connaître :
+	 * Quantité
+	 * Potentiel / jours
+	 * Référence
+	 * Libellé
+	 * Pièce principale / environnement
+	 */
 	$("div#listePieces > table tr.piece").live('click', function(e){
 		var reference = $(this).find("td:first-child").html();
 		var libelle = $(this).find("td:last-child").html();
@@ -71,7 +87,9 @@ $(document).ready(function() {
 		});
 	});
 	
-	// Ajout pièce environnement à la commande
+	/**
+	 * Click sur ajouter la pièce d'environnement à la commande sur le formulaire précédent
+	 */
 	$("#ajoutPieceEnvironnement").live('click', function(e){
 		
 		// récupération des informations de la pièce à ajouter
@@ -97,15 +115,17 @@ $(document).ready(function() {
 		
 		
 		// Ajout d'un champs caché pour la soumission du formulaire
-		var value = reference + "--" + quantite + "--" + potentiel; 
-		$('#piecesEnvironnementHidden').append("<input type='hidden' class='piecesEnv' name='piecesEnv[]' value='" + value + "' />");
+//		var value = reference + "--" + quantite + "--" + potentiel; 
+//		$('#piecesEnvironnementHidden').append("<input type='hidden' class='piecesEnv' name='piecesEnv[]' value='" + value + "' />");
 		
-		addClassPiecesDispo();
+		addClassSelectedPiecesDispo();
 		e.preventDefault();
 	});
 	
 	
-	// Ajout pièce principale à la commande
+	/**
+	 * Click sur ajouter la pièce principale à la commande sur le formulaire précédent
+	 */
 	$("#ajoutPiecePrincipale").live('click', function(e){
 		
 		// récupération des informations de la pièce à ajouter
@@ -118,8 +138,8 @@ $(document).ready(function() {
 		chaine += "<tr>\n";
 		chaine += "<td>" + reference + "</td>\n";
 		chaine += "<td>" + libelle + "</td>\n";
-		chaine += "<td><input type='text' id='tablePieceEnvironementQuantite' name='tablePiecePrincipaleQuantite' value='" + quantite + "' /></td>\n";
-		chaine += "<td><input type='text' id='tableEnvironnementPotentiel' name='tablePieceEnvironnementPotentiel' value='" + potentiel + "' /></td>\n";
+		chaine += "<td><input type='text' class='tablePieceEnvironementQuantite' name='tablePiecePrincipaleQuantite' value='" + quantite + "' /></td>\n";
+		chaine += "<td><input type='text' class='tableEnvironnementPotentiel' name='tablePieceEnvironnementPotentiel' value='" + potentiel + "' /></td>\n";
 		chaine += "<td class='clickable removable principale'></td>\n";
 		chaine += "</tr>\n";
 		
@@ -130,26 +150,29 @@ $(document).ready(function() {
 		$("table#piecePrincipales tbody").append(chaine).hide().fadeIn('slow');
 		
 		
-		// Ajout d'un champs caché pour la soumission du formulaire
-		var value = reference + "--" + quantite + "--" + potentiel; 
-		$('#piecesPrincipalesHidden').append("<input type='hidden' class='piecesPrinc' name='piecesPrinc[]' value='" + value + "' />");
-		
-		addClassPiecesDispo();
+		addClassSelectedPiecesDispo();
 		e.preventDefault();
 	});
 	
 	
-	// Annulation de l'ajout de la pièce à la commande
+	/**
+	 * Click sur annuler la pièce sur le formulaire précédent
+	 */
 	$("#annulerAjoutPieceCommande").live('click', function(e){
 		$("#confirmOverlay").remove();
 		e.preventDefault();
 	});
 	
 	
-	// Suppression de pièce principale
+	/**
+	 * Click sur supprimer une pièce de la commande
+	 */
 	$("td.principale").live('click', function(e){
-		addClassPiecesDispo();
-		alert('');
+		
+		reference = $(this).parent().find("td:first-child").html()
+		supprimerPieceCommande(reference);
+		
+		addClassSelectedPiecesDispo();
 	})
 	
 	
@@ -169,9 +192,14 @@ $(document).ready(function() {
 });
 
 /*
- * Global functions
+ * Global functions   ----------------------------------------------------------
  */
-function addClassPiecesDispo(){
+
+
+/**
+ * Affiche un icone "coché" à toutes les pièces qui ont étés ajoutées à la commande dans la liste des pièces disponibles.
+ */
+function addClassSelectedPiecesDispo(){
 	// création d'une liste des pièces ajoutées dans la commande
 	pieces = new Array();
 	
@@ -190,6 +218,48 @@ function addClassPiecesDispo(){
 			$(value).find('td:last-child').addClass('selected');
 		}
 	});
-}
+};
 
+/**
+ * Supprime la ligne de la pièce dont la référence est passée en parametre dans le tableau de pièces
+ * @param noPiece
+ */
+function supprimerPieceCommande(noPiece){
 	
+	// Suppression de la ligne dans les tableaux de pièces 
+	$('#pieceEnvironnement > tbody > tr').each(function(i, value){
+		reference = $(value).find("td:first-child");
+		if(reference.html() == noPiece) {
+			reference.parent().remove();
+		}
+	});
+	$('#piecePrincipales > tbody > tr').each(function(i, value){
+		reference = $(value).find("td:first-child");
+		if(reference.html() == noPiece) {
+			reference.parent().remove();
+		}
+	});
+	
+};
+
+function ajoutChampsCachesSoumission(){
+	$('#pieceEnvironnement > tbody > tr').each(function(i, value){
+		reference = $(value).find("td:first-child").html();
+		quantite = $(value).find("td:nth-child(3) > input").val();
+		potentiel = $(value).find("td:nth-child(4) > input").val();
+		
+		var value = reference + "--" + quantite + "--" + potentiel; 
+		$('#piecesEnvironnementHidden').append("<input type='hidden' class='piecesEnv' name='piecesEnv[]' value='" + value + "' />");
+
+	});
+
+	$('#piecePrincipales > tbody > tr').each(function(i, value){
+		reference = $(value).find("td:first-child").html();
+		quantite = $(value).find("td:nth-child(3) > input").val();
+		potentiel = $(value).find("td:nth-child(4) > input").val();
+		
+		var value = reference + "--" + quantite + "--" + potentiel; 
+		$('#piecesPrincipalesHidden').append("<input type='hidden' class='piecesPrinc' name='piecesPrinc[]' value='" + value + "' />");
+
+	});
+}
