ca469a8419f2e02fc8a7b55f8a83993da76c3626
Pierrick25 <pierrick.tyrode@gmail.com>
2012-05-18 09:39:31 +0200

    Message d'erreur pour annuler une commande

--- PHPProjetDBB/commande/traitement/annuler_commande.php
+++ PHPProjetDBB/commande/traitement/annuler_commande.php
@@ -14,8 +14,16 @@ if (isset($_GET['num_commande'])){//si on a bien un numéro de commande transmis
 	
 	$query = mysql_query($req) or die('Erreur SQL !<br />'.$req.'<br />'.mysql_error());
 	$donnee = mysql_fetch_assoc($query);
-	if ($donnee['motif_fermeture'] != NULL)     //si le motif de fermeture existe alors elle est déjà fermée
-		echo '<script event=onload>toMenu("La commande n\'a pas pu être annulée : elle est déjà fermée.");</script>';
+	if ($donnee['motif_fermeture'] != NULL)     //si la date de fermeture existe alors elle est déjà fermée
+	{
+		if ($donnee['motif_fermeture'] == "annulée")
+			echo '<script event=onload>toMenu("La commande n\'a pas pu être annulée : elle est déjà annulée.");</script>';
+		else if ($donnee['motif_fermeture'] == "recu")
+			echo '<script event=onload>toMenu("La commande n\'a pas pu être annulée : elle est déjà réceptionnée.");</script>';
+		else
+			echo '<script event=onload>toMenu("La commande n\'a pa pu être annulée.");</script>';
+	}
+		
 	else {                                      //sinon on l'annule
 		$jour = date("Y-m-j");
 		$heure = date("G:i:s");
