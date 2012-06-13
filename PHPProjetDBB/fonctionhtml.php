76d9c0da95756ec16dfb817786a77f33fcde20e4
benito <benoit.stein@hotmail.fr>
2012-06-13 14:35:11 +0200

    Liste commande

--- PHPProjetDBB/fonctionhtml.php
+++ PHPProjetDBB/fonctionhtml.php
@@ -9,18 +9,19 @@ function html_entete_fichier($titre,$fichierstyle,$fichierJava="",$fichierJava1=
 	<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"iconeweb.ico\" />\n
     <meta http-equiv=\"Content-Style-Type\" content=\"text/css\" />\n
    	<link rel=\"stylesheet\" type=\"text/css\" href=\"".$fichierstyle."\" /> \n
-	<link rel=\"stylesheet\" href=\"development-bundle/themes/base/jquery.ui.all.css\"/>
-
+	<link rel=\"stylesheet\" href=\"/include/js/development-bundle/themes/base/jquery.ui.all.css\"/>
+	<link rel=\"stylesheet\" href=\"/include/js/development-bundle/jquery.dataTables.css\"/>
+			
     <meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" /> \n
-    <script src=\"jquery-1.3.1.js\" type=\"text/javascript\"></script> \n
+   <script src=\"../controle/controle.js\" type=\"text/javascript\"></script>
+		<script src=\"/include/js/development-bundle/jquery.min.js\" type=\"text/javascript\"></script>
+		<script src=\"/include/js/development-bundle/ui/jquery.ui.core.js\" type=\"text/javascript\"></script>
+		<script src=\"/include/js/development-bundle/ui/jquery.ui.widget.js\" type=\"text/javascript\"></script>
+		<script src=\"/include/js/development-bundle/ui/jquery.ui.datepicker.js\" type=\"text/javascript\"></script>
+		<script type='text/javascript' src='/include/js/development-bundle/jquery.dataTables.min.js'></script>		
     <script src=\"".$fichierJava."\" type=\"text/javascript\"></script> \n
     <script src=\"".$fichierJava1."\" type=\"text/javascript\"></script> \n
-    <script src=\"../controle/controle.js\" type=\"text/javascript\"></script>
-		<script src=\"development-bundle/jquery-1.6.2.js\" type=\"text/javascript\"></script>
-		<script src=\"development-bundle/ui/jquery.ui.core.js\" type=\"text/javascript\"></script>
-		<script src=\"development-bundle/ui/jquery.ui.widget.js\" type=\"text/javascript\"></script>
-		<script src=\"development-bundle/ui/jquery.ui.datepicker.js\" type=\"text/javascript\"></script>
-		
+    	
 		<script type=\"text/javascript\" >
 		$(function() {
 			$('.datepicker' ).datepicker({
--- PHPProjetDBB/liste/liste.php
+++ PHPProjetDBB/liste/liste.php
@@ -2,6 +2,7 @@
  
 include('../connexion/_connexion.php');
 
+
 require_once("../fonctionhtml.php");  
 
 html_entete_fichier("accueil","../Style.css","script.js","../controle/controle.js");
@@ -9,91 +10,52 @@ html_entete_fichier("accueil","../Style.css","script.js","../controle/controle.j
 mysql_query("SET NAMES UTF8");
 echo("<body>");
 
-//$tab[][] = $tableau;
-?>
 
-	<input type="reset" id="anu" value="Accueil" onclick="document.location.href='../commande/accueil.php';" /> 
+?>
+<input type="reset" id="anu" value="Accueil" onclick="document.location.href='../commande/accueil.php';" /> 
     
-	<form>      
-      	<div id="page">
-			<fieldset>
-				<label> Critère de recherche 1:</label>
-				<select name="table" id="param1" onchange="critere(this.value)">
-					<option select="selected" value="" >Selectionner un critère de recherche</option>
-					<option value="fournisseur">Fournisseur</option>
-					<option value="piece">Piece</option>
-					<option value="silhouette">Silhouette</option>
-					<option value="noCommande">N° commande</option>
-					<option value="typeCommande">Type commande</option>
-					<option value="etatCommande">Etat commande(ouverte/fermée)</option>
-					<option value="date_creation">Période de création</option>
-					<option value="date_reception">Période de réception</option>
-				</select>
-			
-			<div id="date">
-            </div>
-		          
-			</fieldset>
-	
-		</div>	
-		<div id="page2">
-			<fieldset>
-				<label> Critère de recherche 2 :</label>
-				<select name="table" id="param2" onchange="critere2(this.value)">
-					<option select="selected" value="" >Selectionner un critère de recherche : </option>
-					<option value="fournisseur">Fournisseur</option>
-					<option value="piece">Piece</option>
-					<option value="silhouette">Silhouette</option>
-					<option value="noCommande">N° commande</option>
-					<option value="typeCommande">Type commande</option>
-					<option value="etatCommande">Etat commande(ouverte/fermée)</option>
-					<option value="date_creation">Période de création</option>
-					<option value="date_reception">Période de réception</option>
-				</select>
-			
-			<div id="date2">
-            </div>
-		          
-			</fieldset>
-	
-		</div>
-		<div id="page3">
-			<fieldset>
-				<label> Critère de recherche 3 :</label>
-				<select name="table" id="param3" onchange="critere3(this.value)">
-					<option select="selected" value="" >Selectionner un critère de recherche : </option>
-					<option value="fournisseur">Fournisseur</option>
-					<option value="piece">Piece</option>
-					<option value="silhouette">Silhouette</option>
-					<option value="noCommande">N° commande</option>
-					<option value="typeCommande">Type commande</option>
-					<option value="etatCommande">Etat commande(ouverte/fermée)</option>
-					<option value="date_creation">Période de création</option>
-					<option value="date_reception">Période de réception</option>
-				</select>
-				
-			<div id="date3">                
-            </div>
-		          
-			</fieldset>
-	
-		</div>
-	</form>
-			
-		<input type="button" name="Recherche" id="envoi" value="Recherche" onclick="ajout('envoi')" />
-        <div id="content">
-          
-<?php
+   <div id="content">
+   		<div id="table">
+		<table id="tableau_liste">
+		<thead>
+			<tr>
+				<th><label>Numéro commande</label></th>
+				<th><label>Date commande</label></th>
+				<th><label>Fournisseur commande</label></th>
+				<th><label>Type commande</label></th>
+				<th><label>Date réception commande</label></th>
+				<th><label>Identifiant demandeur commande</label></th>
+				<th><label>Motif fermeture</label></th>
+			</tr>
+		</thead>	
+		<?php
+
 
 $sql= "SELECT * FROM COMMANDE";
+$query = mysql_query($sql);
 
-echo "<script>affichage('".$sql."')</script>";
-
-
-/**********************Fin tableau************************/
-
+	echo"<tbody>";
+	
+while($row = mysql_fetch_array($query)){
+	
+	echo("
+			
+			<tr>
+				<th>".$row['no_commande']."</th>
+				<th>".$row['date_commande']."</th>
+				<th>".$row['id_fournisseur']."</th>
+				<th>".$row['code_type_commande']."</th>
+				<th>".$row['date_fermeture']."</th>
+				<th>".$row['id_utilisateur_passe']."</th>
+				<th>".$row['motif_fermeture']."</th>
+			</tr>
+	");
+}
 	?>
-</div>
-        <?php mysql_close(); // deconnexion de la base ?>
+		</tbody>
+		</table>        
+		</div>  	
+	</div>
+    <?php mysql_close(); // deconnexion de la base ?>
 	</body>
 </html>
